1e4d7681cbbeda0b74b660c94a986d237b8a0f74
HgThinker <102933694+HgThinker@users.noreply.github.com>
2024-04-16 22:00:04 +0700

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,1 @@
-# pip install -r requirements.txt
-# conda install -c conda-forge dlib -y
-
-# python template.py
-
-# python setup.py develop
-
 python src/FACE_SWAP/Face_swap_realtime.py --image_name=Biden.jpg --video_name=Biden
